7f47790c878f642c269bf9be717f1e31c32196dc
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-08 13:30:08 -0600

    Update Extract_file_info.py

--- Extract_file_info.py
+++ Extract_file_info.py
@@ -15,9 +15,13 @@ for file in files:
     # Use os.path module to get information about the file
     path = os.path.abspath(file)
     size = os.path.getsize(file)
-    created = datetime.datetime.fromtimestamp(os.path.getctime(file)).strftime('%Y-%m-%d %H:%M:%S')
-    modified = datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
+    created = os.path.getctime(file)
+    modified = os.path.getmtime(file)
 
+    #path = os.path.abspath(file)
+    #size = os.path.getsize(file)
+    #created = datetime.datetime.fromtimestamp(os.path.getctime(file)).strftime('%Y-%m-%d %H:%M:%S')
+    #modified = datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
     
     # Create a dictionary containing the file information
     file_dict = {

1b32125ca9db9bff42b352cbf79d8223cc5d08a5
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-07 18:25:29 -0600

    Update Extract_file_info.py

--- Extract_file_info.py
+++ Extract_file_info.py
@@ -3,23 +3,23 @@
 import os
 import datetime
 
-path = os.getcwd() #Gets current working directory
+path = os.getcwd() # Gets current working directory
 
-#Get a list of files in the current working directory
+# Get a list of files in the current working directory
 files = os.listdir()
 
-file_list = [] #Creates an empty list to hold the dictionaries
+file_list = [] # Creates an empty list to hold the dictionaries
 
-#Loop through each file and add its information to the list
+# Loop through each file and add its information to the list
 for file in files:
-    #Use os.path module to get information about the file
+    # Use os.path module to get information about the file
     path = os.path.abspath(file)
     size = os.path.getsize(file)
     created = datetime.datetime.fromtimestamp(os.path.getctime(file)).strftime('%Y-%m-%d %H:%M:%S')
     modified = datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
 
     
-    #Create a dictionary containing the file information
+    # Create a dictionary containing the file information
     file_dict = {
         "Filename": file,
         "Path": path,
@@ -28,9 +28,9 @@ for file in files:
         "Modified": modified
     }
     
-    #Add the dictionary to the file_list
+    # Add the dictionary to the file_list
     file_list.append(file_dict)
 
-#Print the list of dictionaries
+# Print the list of dictionaries
 print(*file_list, sep="\n")
 

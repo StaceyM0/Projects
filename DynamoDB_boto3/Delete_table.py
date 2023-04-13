4bdf6520d93c236ff981a36dd29d2e544cf76fb5
Stacey Montes <getatmexpress@gmail.com>
2023-04-13 03:13:44 +0000

    Added_code

---
+++ DynamoDB_boto3/Delete_table.py
@@ -0,0 +1,18 @@
+import boto3
+
+# create a DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('Bucket_List') # get a reference to the table
+
+# specify the key of the item to delete
+key = {
+    'Country': 'France', 'City': 'Provence'}
+    
+# delete the item
+response = table.delete_item(
+    Key=key)
+
+# print the response
+print(response)
+print("Item deleted")
---
+++ DynamoDB_boto3/Query_table.py
@@ -0,0 +1,15 @@
+import boto3
+
+# this allows to use Key to define the key condition expression for the query
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.Table('Bucket_List')
+
+## Query the table for items where the 'Country' attribute is 'Mexico'
+response = table.query(
+    KeyConditionExpression=Key('Country').eq('Mexico')
+)
+
+print(response['Items']) # prints the query results

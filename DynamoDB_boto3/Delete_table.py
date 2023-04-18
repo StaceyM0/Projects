069fcdc3d11a47537e7a5c4a75e4890f710d528b
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-17 18:37:24 -0600

    Update Delete_table.py

--- DynamoDB_boto3/Delete_table.py
+++ DynamoDB_boto3/Delete_table.py
@@ -1,7 +1,7 @@
 import boto3 # imports the boto3 library
 
 # create a DynamoDB resource object which will interact with the DynamoDB service
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
 
 table_name = 'Bucket_List' # Adding a variable to table_name
 

47ab0e0904c962127c49306551b539c43116c352
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-17 18:36:31 -0600

    Update Add_items_dynamobo.py

--- DynamoDB_boto3/Add_items_dynamobo.py
+++ DynamoDB_boto3/Add_items_dynamobo.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
 
 table = dynamodb.Table('Bucket_List')
 
